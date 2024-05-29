99b0904ac546fd9c662c58d9b4a0873d9b780b2b
Roman <sarychev@transtelesoft.com>
2024-05-29 13:17:12 +0300

    correction_1

---
+++ dz_2.py
@@ -0,0 +1,144 @@
+from abc import ABC, abstractmethod
+
+
+class IPositionable(ABC):
+    @abstractmethod
+    def get_position(self):
+        pass
+
+class IMovable(ABC):
+    @abstractmethod
+    def GetLocation(self):
+        pass
+
+    @abstractmethod
+    def SetLocation(self, vector):
+        pass
+
+    @abstractmethod
+    def GetVelosity(self):
+        pass
+
+class IRotatable(ABC):
+    @abstractmethod
+    def rotate_left(self):
+        pass
+
+    @abstractmethod
+    def rotate_right(self):
+        pass
+
+    @abstractmethod
+    def get_angle(self):
+        pass
+
+
+class SpaceShip(IMovable):
+    def __init__(self):
+        self.velocity = [1, 1]
+        self.location = [0, 0]
+
+    def GetLocation(self):
+        return self.location
+
+    def SetLocation(self, vector):
+        self.location = vector
+        print(self.location)
+
+    def GetVelosity(self):
+        return self.velocity
+
+
+class SpaceShip1(IMovable):
+    def __init__(self):
+        self.velocity = [2, 2]
+        self.location = [0, 0]
+
+    def GetLocation(self):
+        print(self.location)
+        return self.location
+
+
+    def SetLocation(self, vector):
+        self.location = vector
+
+    def GetVelosity(self):
+        return self.velocity
+
+
+class Vector:
+    pass
+
+
+class PositionableObject(IPositionable):
+    def __init__(self, x, y):
+        self._x = x
+        self._y = y
+
+    def get_position(self):
+        return self._x, self._y
+
+    def set_position(self, x, y):
+        self._x = x
+        self._y = y
+
+
+class MovableObject(PositionableObject, IMovable):
+    def __init__(self, x, y, velocity_x, velocity_y):
+        super().__init__(x, y)
+        self._velocity_x = velocity_x
+        self._velocity_y = velocity_y
+
+    def get_velocity(self):
+        return self._velocity_x, self._velocity_y
+
+    def move(self):
+        x, y = self.get_position()
+        vx, vy = self.get_velocity()
+        self.set_position(x + vx, y + vy)
+
+
+class RotatableObject(PositionableObject, IRotatable):
+    def __init__(self, x, y, angle=0):
+        super().__init__(x, y)
+        self._angle = angle
+
+    def rotate_left(self):
+        self._angle = (self._angle - 90) % 360
+
+    def rotate_right(self):
+        self._angle = (self._angle + 90) % 360
+
+    def get_angle(self):
+        return self._angle
+
+
+# class Move:
+#     def move(self, movable: IMovable):
+#         loc = movable.GetLocation()
+#         vel = movable.GetVelosity()
+#         loc[0] += vel[0]
+#         loc[1] += vel[1]
+#         movable.SetLocation(loc)
+#         # Сложить текущую локацию и скорость
+#
+#
+# class Rotate:
+#     def rotate(self, movable: IMovable):
+#         pass
+#
+#
+# def run():
+#     s = SpaceShip()
+#     s1 = SpaceShip1()
+#     m = Move()
+#
+#     m.move(s1)
+#     s1.GetLocation()
+#     m.move(s1)
+#     s1.GetLocation()
+#     m.move(s1)
+#     s1.GetLocation()
+
+
+# run()
--- tests/test_root.py
+++ tests/test_root.py
@@ -3,6 +3,8 @@ import sys
 sys.path.append('../OTUS_DZ')
 from quadratic_equation import quadratic_equation
 
+from dz_2 import *
+
 
 def test_quadratic():
     assert len(quadratic_equation(1.0, 0, 1.0, 0.0001)) == 0
@@ -17,8 +19,8 @@ def test_multi_one():
 
 
 def test_equal_zero():
+    assert quadratic_equation(0.001, 1, 1, 0.0001) == -1
 
-    assert quadratic_equation(0.001,1,1,0.0001) == -1
 
 def test_type():
     try:
@@ -26,3 +28,43 @@ def test_type():
         assert False
     except:
         assert True
+
+def test_move(self):
+    obj = MovableObject(12, 5, -7, 3)
+    obj.move()
+    self.assertEqual(obj.get_position(), (5, 8))
+def test_move_invalid_position(self):
+    with self.assertRaises(TypeError):
+        obj = MovableObject("invalid", 5, -7, 3)
+        obj.move()
+
+def test_move_invalid_velocity(self):
+    with self.assertRaises(TypeError):
+         obj = MovableObject(12, 5, "invalid", 3)
+         obj.move()
+
+def test_move_unable_to_set_position(self):
+    class NonPositionable:
+        def get_position(self):
+            return 0, 0
+
+    obj = MovableObject(12, 5, -7, 3)
+    obj.set_position = NonPositionable().get_position
+    with self.assertRaises(TypeError):
+        obj.move()
+
+def test_rotate_left(self):
+    obj = RotatableObject(0, 0)
+    obj.rotate_left()
+    self.assertEqual(obj.get_angle(), 270)
+
+def test_rotate_right(self):
+    obj = RotatableObject(0, 0)
+    obj.rotate_right()
+    self.assertEqual(obj.get_angle(), 90)
+
+
+
+
+
+
