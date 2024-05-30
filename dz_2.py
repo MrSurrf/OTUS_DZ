f854c5945b6b2757908c20d6c415b7fd7886874a
Roman <sarychev@transtelesoft.com>
2024-05-30 17:30:31 +0300

    correction_12

--- dz_2.py
+++ dz_2.py
@@ -123,6 +123,27 @@ class RotatableObject(PositionableObject, IRotatable):
         return self._angle
 
 
+class MoveCommand:
+    def __init__(self, movable):
+        self._movable = movable
+
+    def execute(self):
+        self._movable.move()
+
+class RotateCommand:
+    def __init__(self, rotatable, direction):
+        self._rotatable = rotatable
+        self._direction = direction
+
+    def execute(self):
+        if self._direction == 'left':
+            self._rotatable.rotate_left()
+        elif self._direction == 'right':
+            self._rotatable.rotate_right()
+        else:
+            raise ValueError("Error. Use 'left' or 'right'")
+
+
 # class Move:
 #     def move(self, movable: IMovable):
 #         loc = movable.GetLocation()
