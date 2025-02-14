977fd411035c06ca4ea8498e9d5ec7ea09c7162c
Roman <sarychev@transtelesoft.com>
2025-02-14 18:45:04 +0300

    correction_15

--- Project_file.py
+++ Project_file.py
@@ -11,32 +11,21 @@ def get_browser(user_agent):
     ua = parse(user_agent)
     return ua.browser.family
 
-def get_url():
+def get_url(browser_name):
     with open('library.json', 'r') as file:
         browser_dict = json.load(file)
-        url = browser_dict.setdefault(browser_name)
-        print(url)
-        return (handle_browser(url)
+        url = browser_dict.get(browser_name)
+        if url:
+            return handle_browser(url)
+        else:
+            return f"No URL found for browser: {browser_name}", 404
 
-
-
-
-# @app.route('/')
-# def redirect_based_on_browser():
-#     user_agent = request.headers.get('User-Agent')
-#     browser, version = get_browser(user_agent)
-#     print(f"Browser: {browser}, Version: {version}")
-
-
-@app.route('/<path:path>')
-def redirect_based_on_browser_path(path):
+@app.route('/')
+def redirect_based_on_browser():
     user_agent = request.headers.get('User-Agent')
-    browser, version = get_browser(user_agent)
-    ua = parse(user_agent)
-    print(f"Browser: {browser}, Version: {version}")
-    return ua.browser.family, ua.browser.version_string
-
-
+    browser_name = get_browser(user_agent)
+    print(browser_name)
+    return get_url(browser_name)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
